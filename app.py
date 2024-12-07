d5be9fffd656a68a11d11e656a01f2846d7f117e
amosrodrigues <amos.adm.rh@gmail.com>
2024-12-07 10:51:35 -0300

    feat: commit db create flask shell

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ecommerce.db"
 db = SQLAlchemy(app)
 
 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
 @app.route("/")
 def hello_world():
     return "Hello World"
