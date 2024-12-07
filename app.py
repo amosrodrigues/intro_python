68a909fd43513edbb0d9f962339ddb2d18a4515e
amosrodrigues <amos.adm.rh@gmail.com>
2024-12-07 10:29:03 -0300

    feat: implement connection database

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ecommerce.db"
+
+db = SQLAlchemy(app)
 
 
-@app.route("/teste")
+@app.route("/")
 def hello_world():
     return "Hello World"
 
