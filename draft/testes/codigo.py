49e882b27939f687fd181fd7420e41d5077acf13
amosrodrigues <amos.adm.rh@gmail.com>
2025-01-25 21:51:15 -0300

    chore: add draft test codigo 2

--- draft/testes/codigo.py
+++ draft/testes/codigo.py
@@ -1,3 +1,8 @@
 def is_odd(number):
-    "Retorna true se um número é ímpar, senão False."
+    'Retorna true se um número é ímpar, senão False.'
     return number % 2 != 0
+
+
+def divide(a_number, other_number):
+    'Retorna a divisão de a number por orher number'
+    return a_number / other_number
