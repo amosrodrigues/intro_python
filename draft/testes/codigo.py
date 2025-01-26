3dcb92db492278921d59927b59410b13057213a2
amosrodrigues <amos.adm.rh@gmail.com>
2025-01-25 22:29:03 -0300

    chore: add draft test codigo 3

--- draft/testes/codigo.py
+++ draft/testes/codigo.py
@@ -6,3 +6,30 @@ def is_odd(number):
 def divide(a_number, other_number):
     'Retorna a divisão de a number por orher number'
     return a_number / other_number
+
+
+def get_most_ordered_dish_per_costumer(orders, customer):
+    max_amount = 0
+    most_ordered = ""
+    customer_dishes = {}
+
+    for order in orders:
+        if order["customer"] == customer:
+            customer_dishes[order["order"]] = (
+                customer_dishes.get(order["order"], 0) + 1
+            )
+            if customer_dishes[order["order"]] >= max_amount:
+                max_amount = customer_dishes[order["order"]]
+                most_ordered = order["order"]
+    return most_ordered
+
+
+def get_order_frequency_per_costumer(orders, customer, order):
+    counter = 0
+    for current_order in orders:
+        if (
+            current_order["customer"] == customer
+            and current_order["order"] == order
+        ):
+            counter += 1
+    return counter
